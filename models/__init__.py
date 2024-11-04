62ec72bc11b298026818b208c05faae8b92f77f3
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-04 22:09:44 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,10 @@
 
+# models/__init__.py
+
+# Import your models here
+from .project import Project
+# You can add more models as needed
+# from .another_model import AnotherModel
+
+# Optionally, create a list of all models for easier management
+models = [Project]
