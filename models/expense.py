8778b0a1f364582c4901a70f0fe56cb7315d2669
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-04 16:14:48 +0100

    Create expense.py

---
+++ models/expense.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Expense(db.Model):
+    __tablename__ = 'expenses'
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
+    amount = db.Column(db.Float, nullable=False)
+    contributor = db.Column(db.String(100))
+    comment = db.Column(db.String(255))
+    currency = db.Column(db.String(3))
+    status = db.Column(db.String(50))  # Paid or Pending
