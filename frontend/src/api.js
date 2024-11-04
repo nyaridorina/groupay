56655ad8bbad6a02ce0c72822647597b9e9a95af
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-04 20:45:49 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://your-backend-url.onrender.com/api",
+  baseURL: "https://groupay.onrender.com",
 });
 
 export const fetchProjects = () => api.get("/projects");
