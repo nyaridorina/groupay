813cd01689fd6ca9feb8695821690b0272432a66
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-04 21:49:23 +0100

    Update project.py

--- models/project.py
+++ models/project.py
@@ -1,10 +1,13 @@
-from app import db
+# models/project.py
+from backend import db  # Import db from backend instead of app
 
+# Define your model here
 class Project(db.Model):
-    __tablename__ = 'projects'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
     goal_amount = db.Column(db.Float, nullable=False)
     currency = db.Column(db.String(3), nullable=False)
     background_image_url = db.Column(db.String(255))
     is_archived = db.Column(db.Boolean, default=False)
+
