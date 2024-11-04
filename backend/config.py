1194af38d6c22c73e25102cad3aa62862ed2c028
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-04 16:33:04 +0100

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
