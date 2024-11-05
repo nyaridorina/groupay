09c3b195b15756a664264e5d7e9f80fd5ade39c1
nyaridorina <42637093+nyaridorina@users.noreply.github.com>
2024-11-05 15:27:59 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from routes.project_routes import project_bp
 from routes.expense_routes import expense_bp
-from backend import create_app
+from backend import create_app  # Ensure correct import
 
 # Create the Flask app instance using the factory function
 app = create_app()
 
 if __name__ == "__main__":
-    # Get the port from the environment variable (Render provides this) or default to 5000 for local development
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
